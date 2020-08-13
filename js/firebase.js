d7c54c0fbf26502f8a8416f1a6cb0336b3d9cbc1
askanniething <39163490+askanniething@users.noreply.github.com>
2020-08-13 16:29:59 -0400

    Added the "Advertise with us" section

---
+++ js/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCJhTOz1svs8XRG3Pp-nrFIWJTy7zNbG-Q",
+    authDomain: "something-unique-78d6c.firebaseapp.com",
+    databaseURL: "https://something-unique-78d6c.firebaseio.com",
+    projectId: "something-unique-78d6c",
+    storageBucket: "something-unique-78d6c.appspot.com",
+    messagingSenderId: "985635920948",
+    appId: "1:985635920948:web:8b1baf53300f147f27be61",
+    measurementId: "G-2TDRRS9Z1M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase);
